5955c2055eca4f629926a7748d1e9cd476306142
MdSaifurRahaman <md.saifurrahaman.cse@gmail.com>
2021-12-19 19:08:53 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,138 @@
+const express = require("express");
+const app = express();
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qc9ei.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("aerialAspects");
+        const productsCollection = database.collection("products");
+        const reviewsCollection = database.collection("reviews");
+        const ordersCollection = database.collection("orders");
+        const usersCollection = database.collection("users");
+
+        //     Get api to get all products data
+        app.get("/products", async (req, res) => {
+            const cursor = productsCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+        //POST Api to post new product
+        app.post("/products", async (req, res) => {
+            const product = req.body;
+            const result = await productsCollection.insertOne(product);
+            res.json(result);
+        });
+
+        // Get Api to get single product
+        app.get("/products/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productsCollection.findOne(query);
+            res.json(product);
+        });
+
+        //POST Api to post user booking data
+        app.post("/orders", async (req, res) => {
+            const order = req.body;
+            // console.log(user);
+            const result = await ordersCollection.insertOne(order);
+            res.json(result);
+        });
+
+        // GET api to get users data
+        app.get("/users/:email", async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === "admin") {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        });
+
+        // POST api to post users data
+        app.post("/users", async (req, res) => {
+            const user = req.body;
+            // console.log(user);
+            const result = await usersCollection.insertOne(user);
+            res.json(result);
+        });
+
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updatedDoc = { $set: user }
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
+            res.json(result)
+        })
+
+        // PUT api to make admin
+        app.put("/users", async (req, res) => {
+            const user = req.body;
+            console.log("put", user);
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: "admin" } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        });
+
+        //GET Api to get all user booked orders
+        app.get("/orders", async (req, res) => {
+            const cursor = ordersCollection.find({});
+            const orderedData = await cursor.toArray();
+            res.send(orderedData);
+        });
+
+        // Delete api to cancel orderd events
+        app.delete("/orders/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            res.json(result);
+        });
+
+        //     Get api to get all reviews
+        app.get("/reviews", async (req, res) => {
+            const cursor = reviewsCollection.find({});
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+
+        //POST Api to post user review
+        app.post("/reviews", async (req, res) => {
+            const review = req.body;
+            const result = await reviewsCollection.insertOne(review);
+            res.json(result);
+        });
+    } finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+    res.send("Aerial Aspects Server Running");
+});
+
+app.listen(port, () => {
+    console.log("this is running", port);
+});
